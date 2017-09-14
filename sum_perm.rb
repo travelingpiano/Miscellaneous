e3293211f01b222617e253e3f8407d91bc6e982e
travelingpiano <lihsuanlu@gmail.com>
2017-09-14 16:20:11 -0700

    first commit, working solution of permutation sum problem

---
+++ sum_perm.rb
@@ -0,0 +1,26 @@
+#assuming array only contains positive numbers
+def sum_perm(arr,target)
+  # if arr.length == 1
+  #   return arr[0]%target == 0 ? [[arr[0]]*target/arr[0]] : []
+  # end
+  # prev_result = sum_perm(arr[0...-1],target)
+  new_result = []
+  test_queue = arr.map{|el| el = [el]}
+  until test_queue.empty?
+    test_case = test_queue.shift
+    arr.each do |el|
+      if test_case.reduce(:+) + el == target
+        new_result.push(test_case + [el])
+      elsif test_case.reduce(:+) + el < target
+        test_queue.push(test_case + [el])
+      end
+    end
+  end
+  return new_result
+end
+
+p(sum_perm([2,3],10))
+
+p(sum_perm([3,7],10)) #expect [3,7], [7,3]
+
+p(sum_perm([2,3,5,7],10)) #expect [3,7], [7,3], [2,2,2,2,2], [2,3,3,2], [2,2,3,3]...
